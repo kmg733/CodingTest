bf0978733f142add2bb4b780015efc2fee88cf70
kmg733 <mang906@naver.com>
2022-01-02 06:04:18 +0900

    Lab Coding Study Group Week4(D)

--- BoJ/python/연구실 스터디그룹/SILVER 2/1012_유기농 배추(복습필요).py
+++ BoJ/python/연구실 스터디그룹/SILVER 2/1012_유기농 배추(복습필요).py
@@ -4,43 +4,44 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# 너비우선탐색
-def bfs(f, x, y):
-    que = deque()
-    que.append([x, y])
-    f[x][y] = 0
-    
-    # 큐가 비어있지 않다면 반복실행
-    while que:
-        x2, y2 = que.popleft()
-        for i in range(4):
-            x2 = x + dxy[i][0]
-            y2 = y + dxy[i][1]
-            if x2 < 0 or x2 >= n or y2 < 0 or y2 >= m:
-                continue
-            if f[x2][y2] == 1:
-                f[x2][y2] = 0
-                que.append([x2, y2])
-    return
-
+t = int(input())
 dxy = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-t = int(input())
+# 너비우선 탐색
+def bfs(i, j, field):
+    que = deque()
+    que.append((i, j))
+    field[i][j] = 0
 
+    while que:
+        y, x = que.popleft()        
 
-# 배추밭 생성
+        for i in range(4):
+            my = y + dxy[i][0]
+            mx = x + dxy[i][1]
+            if my < 0 or my >= n or mx < 0 or mx >= m:
+                continue
+            if field[my][mx] == 1:
+                field[my][mx] = 0
+                que.append((my, mx))
+
+    return
+
+# 배추밭 만들기
 for _ in range(t):
-    count = 0
-    n, m, k = map(int, input().split())
-    field = [[0 for _ in range(m)] for _ in range(n)]
+    m, n, k = map(int, input().split())
+    field = [[0] * m for _ in range(n)]
+    cnt = 0
 
-    for i in range(k):
+    # 배추를 심은곳 입력받기
+    for _ in range(k):
         x, y = map(int, input().split())
-        field[x][y] = 1
+        field[y][x] = 1
     
     for i in range(n):
         for j in range(m):
             if field[i][j] == 1:
-                bfs(field, i, j)
-                count += 1
-    print(count)
+                bfs(i, j, field)
+                cnt += 1
+    
+    print(cnt)
