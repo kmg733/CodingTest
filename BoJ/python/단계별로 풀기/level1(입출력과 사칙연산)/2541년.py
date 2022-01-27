d15ecfd7d929bac08188b0bdc6739291190d9fef
kmg733 <mang906@naver.com>
2022-01-27 21:43:31 +0900

    BoJ level1 Added Problem Solve(python)

---
+++ BoJ/python/단계별로 풀기/level1(입출력과 사칙연산)/2541년.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/18108
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+print(a-543)
---
+++ BoJ/python/단계별로 풀기/level1(입출력과 사칙연산)/surprise.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/10926
+import sys
+input = sys.stdin.readline
+
+id = input().strip()
+print(f"{id}??!")
