5d2f18cda5a896420e8f6abb8b10fe328f8328ec
unknown <mink906@smail.kongju.ac.kr>
2022-02-17 01:56:58 +0900

    Programmers Heap level2 Done(Java)

--- Programmers/Java_lan/level2/더_맵게.java
+++ Programmers/Java_lan/level2/더_맵게.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class 더_맵게 {
+    public static void main(String[] args) {
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 2;
+        System.out.println(solution(scoville, K));
+    }
     
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        for(int sco : scoville) {
+            heap.add(sco);
+        }
+        
+        while(heap.peek() <= K) {
+            if(heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+            heap.add(a + b * 2);
+            answer++;
+        }
+        return answer;
+    }
 }
 
 // 알고리즘 종류
