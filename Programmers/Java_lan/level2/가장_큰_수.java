f3eb9dd4625b230b8a80b15a24ebaa96a334744e
kmg733 <mang906@naver.com>
2022-02-17 02:56:02 +0900

    Programmers Sort level1 ~ level2 Done(Java)

---
+++ Programmers/Java_lan/level2/가장_큰_수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class 가장_큰_수 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 30, 34, 5, 9};
+        System.out.println(solution(numbers));
+    }
+    public static String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] str = new String[numbers.length];        
+        for(int i = 0; i < str.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(str, new Comparator<String>() {
+           public int compare(String o1, String o2) {
+               return (o2 + o1).compareTo(o1 + o2);
+           } 
+        });
+        
+        if(str[0].equals("0"))
+            return "0";
+        
+        return String.join("", str);
+    }
+}
